6507269a5dbc48ddcc94801d3a4fb750ee957fda
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-19 16:35:26 -0700

    added username to to User model

--- server/models/User.js
+++ server/models/User.js
@@ -14,6 +14,11 @@ const userSchema = new Schema({
       required: true,
       trim: true
     },
+    username: {
+      type: String,
+      required: true,
+      trim: true
+    },
     email: {
       type: String,
       required: true,
