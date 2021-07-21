4161742b80e95dd9802b689dbb398f1badabf3aa
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-21 14:03:42 -0700

    created User model

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,3 @@
 const User = require('./User');
-const Product = require('./Product');
-const Print = require('./Print');
-const Category = require('./Category');
-const Order = require('./Order');
-const Customer = require('./Customer');
 
-module.exports = { User, Product, Print, Category, Order, Customer };
+module.exports = { User };
