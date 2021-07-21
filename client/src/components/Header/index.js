455234dfffec33bd425423d24f9363fc5d47eb11
Kedakai <nicoleteeth@ucla.edu>
2021-07-20 23:31:37 -0700

    add contact form to about page, set up components

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,1 +1,40 @@
 // Logo, Navigation: About, Log In, Sign Up
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+
+const Header = () => {
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  return (
+    <header className="bg-secondary mb-4 py-2 flex-row align-center">
+      <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <Link to="/">
+          <h1>Dark Room</h1>
+        </Link>
+
+        <nav className="text-center">
+            <Link to="/about">About</Link>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Profile</Link>
+              <a href="/" onClick={logout}>
+                Log Out
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Log In</Link>
+              <Link to="/signup">Sign Up</Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
