8e0b65ac41161f3d908db3b15908d1a44eb18366
Kedakai <nicoleteeth@ucla.edu>
2021-07-20 23:35:36 -0700

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
-// import Footer from './components/Footer';
+import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
