61b6bfa1434c5350c42ba1194185f1f84dcbc59e
Kedakai <nicoleteeth@ucla.edu>
2021-07-21 22:42:41 -0700

    set up quote API

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,7 @@ import { QUERY_POSTS, QUERY_ME_BASIC } from '../utils/queries';
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
 import PostForm from '../components/PostForm';
+import { getFragmentQueryDocument } from '@apollo/client/utilities';
 
 
 const Home = () => {
@@ -19,13 +20,41 @@ const Home = () => {
   console.log(posts);
 
   const loggedIn = Auth.loggedIn();
+
+  let quoteString = "";
+
+  /*Api Call*/
+  getQuote() {
+
+    var axios = require("axios").default;
+
+    var options = {
+      method: 'GET',
+      url: 'https://dad-jokes.p.rapidapi.com/random/joke',
+      headers: {
+        'x-rapidapi-key': 'fd3c2bc085msh62049fe3043d594p1cd891jsn7d8a7cbcecf8',
+        'x-rapidapi-host': 'dad-jokes.p.rapidapi.com'
+      }
+    };
+    
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+      quoteString = response.data.body.setup + " " + response.data.body.punchline;
+    }).catch(function (error) {
+      console.error(error);
+    });
  
 
   return (
     <main>
       <div className="flex-row justify-space-between">
-        {/* Quote of the Day
-        Reviews */}
+        {/* Quote of the Day */}
+        <h2> Dark Room: Quote of the Day </h2>
+        <div>
+          {`${quoteString}`}
+        </div>
+
+        {/* Reviews */}
         {loggedIn && (
           <div className="col-12 mb-3">
             <PostForm />
